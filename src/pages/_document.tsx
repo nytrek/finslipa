d56b5e1921366402859d9aba3e3eaf43b994a785
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2024-02-28 10:18:51 +0100

    remove relative class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="sv-SE">
       <Head />
-      <body className="relative">
+      <body>
         <Main />
         <NextScript />
       </body>
