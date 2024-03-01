c67daae97a708d9fbc63171579f97b2671b2071b
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2024-03-01 13:37:54 +0100

    update portfolio section to support local image through import

---
+++ src/components/portfolio/data.tsx
@@ -0,0 +1,39 @@
+import five from "../../../public/five.png";
+import four from "../../../public/four.png";
+import one from "../../../public/one.png";
+import six from "../../../public/six.png";
+import three from "../../../public/three.png";
+import two from "../../../public/two.png";
+
+export const portfolio = [
+  {
+    id: 1,
+    name: "Fitness applikation",
+    image: one,
+  },
+  {
+    id: 2,
+    name: "Pomodoro koncept",
+    image: two,
+  },
+  {
+    id: 3,
+    name: "E-handelsbutik",
+    image: three,
+  },
+  {
+    id: 4,
+    name: "Fitness tracker",
+    image: four,
+  },
+  {
+    id: 5,
+    name: "Valuta konverterare",
+    image: five,
+  },
+  {
+    id: 6,
+    name: "Elegant affärsmodel",
+    image: six,
+  },
+];
--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/utils/cn";
+import Image from "next/image";
+import { portfolio } from "./data";
 
 const Container: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   children,
@@ -6,6 +8,9 @@ const Container: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   return <div className="px-4 lg:py-12">{children}</div>;
 };
 
+/**
+ * @see https://www.npmjs.com/package/@storybook/nextjs
+ */
 export const Portfolio: React.FC<React.HTMLAttributes<HTMLElement>> = ({
   className,
   ...props
@@ -21,78 +26,20 @@ export const Portfolio: React.FC<React.HTMLAttributes<HTMLElement>> = ({
           Portfölj
         </h2>
         <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3">
-          <div className="relative rounded-2xl">
-            <div className="absolute inset-x-0 top-0 flex h-full items-end justify-end overflow-hidden rounded-lg p-4">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black opacity-50"
-              />
-              <p className="relative text-lg font-semibold text-white">
-                Fitness applikation
-              </p>
+          {portfolio.map((item) => (
+            <div className="relative rounded-2xl" key={item.name}>
+              <div className="absolute inset-x-0 top-0 flex h-full items-end justify-end overflow-hidden rounded-lg p-4">
+                <div
+                  aria-hidden="true"
+                  className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black opacity-50"
+                />
+                <p className="relative text-lg font-semibold text-white">
+                  {item.name}
+                </p>
+              </div>
+              <Image alt="project" className="w-full" src={item.image} />
             </div>
-            <img alt="project" className="w-full" src="/one.png" />
-          </div>
-          <div className="relative rounded-2xl">
-            <div className="absolute inset-x-0 top-0 flex h-full items-end justify-end overflow-hidden rounded-lg p-4">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black opacity-50"
-              />
-              <p className="relative text-lg font-semibold text-white">
-                Pomodoro koncept
-              </p>
-            </div>
-            <img alt="project" className="w-full" src="/two.png" />
-          </div>
-          <div className="relative rounded-2xl">
-            <div className="absolute inset-x-0 top-0 flex h-full items-end justify-end overflow-hidden rounded-lg p-4">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black opacity-50"
-              />
-              <p className="relative text-lg font-semibold text-white">
-                E-handelsbutik
-              </p>
-            </div>
-            <img alt="project" className="w-full" src="/three.png" />
-          </div>
-          <div className="relative rounded-2xl">
-            <div className="absolute inset-x-0 top-0 flex h-full items-end justify-end overflow-hidden rounded-lg p-4">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black opacity-50"
-              />
-              <p className="relative text-lg font-semibold text-white">
-                Fitness tracker
-              </p>
-            </div>
-            <img alt="project" className="w-full" src="/four.png" />
-          </div>
-          <div className="relative rounded-2xl">
-            <div className="absolute inset-x-0 top-0 flex h-full items-end justify-end overflow-hidden rounded-lg p-4">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black opacity-50"
-              />
-              <p className="relative text-lg font-semibold text-white">
-                Valuta konverterare
-              </p>
-            </div>
-            <img alt="project" className="w-full" src="/five.png" />
-          </div>
-          <div className="relative rounded-2xl">
-            <div className="absolute inset-x-0 top-0 flex h-full items-end justify-end overflow-hidden rounded-lg p-4">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black opacity-50"
-              />
-              <p className="relative text-lg font-semibold text-white">
-                Elegant affärsmodel
-              </p>
-            </div>
-            <img alt="project" className="w-full" src="/six.png" />
-          </div>
+          ))}
         </div>
       </section>
     </Container>
